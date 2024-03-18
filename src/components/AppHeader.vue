976434c180387fd96902243ea4dadea613114c60
go-web5 <go.web.creator@gmail.com>
2024-03-18 17:33:03 +0900

    ナビゲーション追加に伴うスタイル調整など

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <header :style="{ 'background-color': backgroundColor }">
-    <h1 class="lesson-1">Vue Lesson</h1>
-    <p class="lesson-1__text">Vue Lesson 1 : ヘッダーのコンポーネントを作成して、それを画面A,B,Cに実装する</p>
+    <div>
+      <h1 class="lesson-1">Vue Lesson</h1>
+      <p class="lesson-1__text">Vue Lesson 1 : ヘッダーのコンポーネントを作成して、それを画面A,B,Cに実装する</p>
+    </div>
   </header>
 </template>
 
